8f9be5cc892946ec5ab3110646d1053fd271fa49
Alex Johnson <alex.johnson293@gmail.com>
2022-10-31 16:32:48 -0700

    adding UI elements
    Co-authored-by: Richelle Thorpe <richelle.ak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,40 @@ DestinationBook.prototype.findLocation = function(id) {
   }
 }
 
+//UI Logic
 
-//ui logik
+let destinationBook = new DestinationBook();
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputLocation = document.querySelector("input#inputLocation").value;
+  const inputLandmark = document.querySelector("input#inputLandmark").value;
+  const inputTimeOfYear = document.querySelector("input#inputTimeOfYear").value;
+  const inputNotes = document.querySelector("input#inputNotes").value;
+  let newLocation = new Location(inputLocation, inputLandmark, inputTimeOfYear, inputNotes);
+  destinationBook.addLocation(newLocation);
+  console.log('location appended successfully!', newLocation);
+  printLocations(destinationBook);
+  document.querySelector("div#location-details").removeAttribute("class");
+}
+
+function printLocations(destinationBookToDisplay) {
+  let locationDiv = document.querySelector("div#location-details");
+  locationDiv.innerText = null;
+  const ul = document.createElement("ul");
+  console.log('beginning forEach!');
+  Object.keys(destinationBookToDisplay.locations).forEach(function(key) {  
+    const location = destinationBookToDisplay.findLocation(key);
+    const li = document.createElement("li");
+    //this line is breaking things fix what is getting appended?
+    li.append(destinationBookToDisplay.object);
+    li.setAttribute("id", location.id);
+    ul.append(li);
+    console.log('appended:', li)
+  });
+  locationDiv.append(ul);
+}
+
 function Location(location, landmarks, timeOfYear, notes) {
   this.location = location;
   this.landmarks = landmarks;
@@ -28,8 +60,13 @@ function Location(location, landmarks, timeOfYear, notes) {
   this.notes = notes;
 }
 
-let destinationBook1 = new DestinationBook();
-let testLocation1 = new Location("Spain", "The Alhambra", "Winter", "Paella is delicious!");
-let testLocation2 = new Location("Arctic", "Northern Lights", "Winter", "Brrrr!");
-destinationBook1.addLocation(testLocation1);
-destinationBook1.addLocation(testLocation2);
+window.addEventListener("load", function() {
+  document.querySelector("form#new-location").addEventListener("submit", handleFormSubmission);
+})
+
+
+// let destinationBook1 = new DestinationBook();
+// let testLocation1 = new Location("Spain", "The Alhambra", "Winter", "Paella is delicious!");
+// let testLocation2 = new Location("Arctic", "Northern Lights", "Winter", "Brrrr!");
+// destinationBook1.addLocation(testLocation1);
+// destinationBook1.addLocation(testLocation2);
